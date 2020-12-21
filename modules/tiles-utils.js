c2482938cc5c7fd0227d73e62e9177265a31300a
Michele Fenu <michelefenu@gmail.com>
2020-12-21 10:50:05 +0100

    Try Part 2

--- modules/tiles-utils.js
+++ modules/tiles-utils.js
@@ -1,8 +1,8 @@
 const findBordersIds = (tiles) => {
   const borderTiles = []
 
-  for(let tile of tiles) {
-    if(isBorderTile(tile.id, tiles)) {
+  for (let tile of tiles) {
+    if (isBorderTile(tile.id, tiles)) {
       borderTiles.push(tile.id)
     }
   }
@@ -11,23 +11,74 @@ const findBordersIds = (tiles) => {
 }
 
 const buildPhoto = (tiles) => {
-  for(let tile of tiles) {
-  let matchingBorders = 0
+  for (const [index, tile] of tiles) {
+    tile.found = true
+    for (let [seachIndex, searchTile] of tiles) {
+      if (index === seachIndex) { continue }
 
-    for(let border of tile.borders) {
-      matchingBorders += tiles.filter(x => x.id !== tile.id).filter(x => matchesAnyBorder(border, x.borders)).length
+      // check top
+      const topBorder = tile.borders[0]
+      let i = 0
+      while (i < 8) {
+        if (searchTile[searchTile.length - 1] === topBorder) {
+
+          break
+        }
+        tiles[searchIndex] = flipH(tiles[searchIndex])
+        if (searchTile[searchTile.length - 1] === topBorder) {
+          tile.neighbors = tile.neighbors ? tile.neighbors.push({pos: 'top', tileId: searchTile.id}) || [{pos: 'top', tileId: searchTile.id}]
+          break
+        }
+        i++
+      }
+
+      // check right
+      const rightBorder = tile.borders[0]
+
+      // check bottom
+      const bottomBorder = tile.borders[0]
+
+
+      // check left
+      const leftBorder = tile.borders[0]
+
     }
-    console.log(matchingBorders)
   }
+}
+
+const findMatchingTile = (id, border, tiles) => {
+  return !!tiles.filter(x => x.id !== id).filter(x => matchesAnyBorder(border, x.borders)).length
+}
+
+const rotateCC = (tile) => {
+  const rotatedTile = []
+
+  for (let i = 0; i < tile[0].length; i++) {
+    let row = ''
+    for (let line of tile) {
+      row = line[i] + row
+    }
+    rotatedTile.push(row)
+  }
+
+  return rotatedTile
+}
+
+const flipV = (tile) => {
+  return tile.reverse()
+}
+
+const flipH = (tile) => {
+  return tile.map(x => x.split('').reverse('').join(''))
 }
 
 const isBorderTile = (id, tiles) => {
   const borders = tiles.filter(x => x.id === id)[0].borders
 
   let matchingBorders = 0
-  for(let border of borders) {
+  for (let border of borders) {
     const borderMatch = !!tiles.filter(x => x.id !== id).filter(x => matchesAnyBorder(border, x.borders)).length
-    if(borderMatch) {
+    if (borderMatch) {
       matchingBorders++
     }
   }
@@ -43,9 +94,9 @@ const getBorders = (tile) => {
   let leftBorder = ''
   let rightBorder = ''
   let topBorder = tile[0]
-  let bottomBorder = tile[tile.length -1]
+  let bottomBorder = tile[tile.length - 1]
 
-  for(let row of tile) {
+  for (let row of tile) {
     leftBorder += row[0]
     rightBorder += row[row.length - 1]
   }
@@ -54,7 +105,7 @@ const getBorders = (tile) => {
 }
 
 module.exports = {
-  findBordersIds, 
+  findBordersIds,
   getBorders,
   buildPhoto
 }
