891e1fc8cf44539fc2f4c7dbb045f12f3247b498
Michele Fenu <michelefenu@gmail.com>
2020-12-28 12:00:26 +0100

    Solve Day 23

--- modules/cup-stack.js
+++ modules/cup-stack.js
@@ -15,7 +15,6 @@ const runSimulation = (cups, moves) => {
     const pickedUpCups = [cupStack.removeAt(1), cupStack.removeAt(1), cupStack.removeAt(1)]
 
     let destination = cupStack.getHead().element > 1 ? cupStack.getHead().element - 1 : maxValue
-
     while (!cupStack.isPresent(destination)) {
       destination = destination > 1 ? destination - 1 : maxValue
     }
@@ -40,81 +39,84 @@ const runSimulation = (cups, moves) => {
  * @param {number} moves 
  */
 const runExtremeSimulation = (cups, moves) => {
-  /* const MAX_VALUE = 1000000
-  let cupStack = [...Array(MAX_VALUE).keys()].slice(cups.length + 1)
-  cupStack =[...cups, ...cupStack] */
+  const cupStack = new CircularLinkedList()
 
-  let cupStack = cups
-  const MAX_VALUE = cups.length
-
-  let currentCup = cups[0]
-
-  while (moves--) {
-    console.log('---', 10 - moves, '---')
-    console.log(cupStack)
-    let currentCupIndex = cupStack.indexOf(currentCup)
-    const pickedUpCups = []
-    pickedUpCups.push(...cupStack.splice(currentCupIndex + 1 < cupStack.length ? currentCupIndex + 1 : 0, 1))
-    currentCupIndex = cupStack.indexOf(currentCup)
-    pickedUpCups.push(...cupStack.splice(currentCupIndex + 1 < cupStack.length ? currentCupIndex + 1 : 0, 1))
-    currentCupIndex = cupStack.indexOf(currentCup)
-    pickedUpCups.push(...cupStack.splice(currentCupIndex + 1 < cupStack.length ? currentCupIndex + 1 : 0, 1))
-    currentCupIndex = cupStack.indexOf(currentCup)
-
-    console.log(pickedUpCups)
-
-    let destination = cupStack[currentCupIndex] > 1 ? cupStack[currentCupIndex] - 1 : MAX_VALUE
-
-    while (destination === currentCup || cupStack.indexOf(destination) === -1) {
-      destination = destination > 1 ? destination - 1 : MAX_VALUE
-    }
-
-    console.log(destination)
-
-
-    let destinationIndex = cupStack.indexOf(destination)
-
-    let before = [...cupStack.slice(0, destinationIndex + 1)]
-    let after = [...cupStack.slice(destinationIndex + 1)]
-    cupStack = [...before, ...pickedUpCups, ...after]
-
-    currentCupIndex = cupStack.indexOf(currentCup)
-    currentCup = cupStack[currentCupIndex + 1 < cupStack.length ? currentCupIndex + 1 : 0]
+  for (let cup of cups) {
+    cupStack.append(cup)
   }
 
-  const headIndex = cupStack.indexOf(1)
-  const n = cupStack.length
-  console.log(cupStack[(headIndex + 1 % n + n) % n], cupStack[(headIndex + 2 % n + n) % n])
+  cupStack.fillUntil(1000000)
+  const maxValue = cupStack.length
+  
+  while (moves--) {
+    const pickedUpCups = [cupStack.removeAfterHead(), cupStack.removeAfterHead(), cupStack.removeAfterHead()]
+
+    let destination = cupStack.getHead().element > 1 ? cupStack.getHead().element - 1 : maxValue
+
+    while (!cupStack.isPresentBySearch(destination)) {
+      destination = destination > 1 ? destination - 1 : maxValue
+    }
+
+    cupStack.insertAfter(destination, pickedUpCups[2].element)
+    cupStack.insertAfter(destination, pickedUpCups[1].element)
+    cupStack.insertAfter(destination, pickedUpCups[0].element)
+
+    cupStack.setHead(cupStack.getHead().next)
+  }
+
+  return cupStack.getElementByValue(1).next.element * cupStack.getElementByValue(1).next.next.element
 }
 
 class CircularLinkedList {
-  #length
+  length
   #head
+  #searchByValue
 
   constructor() {
-    this.head = null
+    this.#head = null
     this.length = 0
+    this.#searchByValue = []
   }
 
   append(element) {
     const node = new Node(element)
     let current
 
-    if (this.head === null) {
-      this.head = node
+    if (this.#head === null) {
+      this.#head = node
     } else {
       current = this.getElementAt(this.length - 1)
       current.next = node
     }
 
-    node.next = this.head
+    node.next = this.#head
 
+    this.#searchByValue[element] = node
     this.length++
+  }
+
+  /**
+   * 
+   * @param {number} max fill the list until max is reached
+   */
+  fillUntil(max) {
+    let tail = this.getElementAt(this.length - 1)
+
+    for (let i = this.length + 1; i <= max; i++) {
+      let current = new Node(i)
+      tail.next = current
+      tail = current
+      this.length++
+      this.#searchByValue[i] = tail
+
+    }
+    tail.next = this.#head
+    this.#searchByValue[tail.element] = tail
   }
 
   getElementAt(index) {
     if (index >= 0 && index <= this.length) {
-      let node = this.head
+      let node = this.#head
       for (let i = 0; i < index && node != null; i++) {
         node = node.next
       }
@@ -126,17 +128,17 @@ class CircularLinkedList {
   insert(element, index) {
     if (index >= 0 && index <= this.length) {
       const node = new Node(element);
-      let current = this.head;
+      let current = this.#head;
 
       if (index === 0) {
-        if (this.head === null) {
-          this.head = node
-          node.next = this.head
+        if (this.#head === null) {
+          this.#head = node
+          node.next = this.#head
         } else {
           node.next = current
           current = this.getElementAt(this.length)
-          this.head = node
-          current.next = this.head
+          this.#head = node
+          current.next = this.#head
         }
       } else {
         const previous = this.getElementAt(index - 1)
@@ -150,18 +152,33 @@ class CircularLinkedList {
     return false
   }
 
+  /**
+   * Inserts a new node after an element
+   * @param {} element 
+   * @param {*} nodeValue 
+   */
+  insertAfter(element, nodeValue) {
+    const elementNode = this.getElementByValue(element)
+    const node = new Node(nodeValue)
+
+    node.next = elementNode.next
+    elementNode.next = node 
+
+    this.#searchByValue[nodeValue] = node
+  }
+
   removeAt(index) {
     if (index >= 0 && index < this.length) {
-      let current = this.head
+      let current = this.#head
 
       if (index === 0) {
         if (this.length === 1) {
-          this.head = undefined
+          this.#head = undefined
         } else {
           const removed = head
           current = this.getElementAt(this.length - 1)
-          this.head = this.head.next
-          current.next = this.head
+          this.#head = this.#head.next
+          current.next = this.#head
           current = removed
         }
       } else {
@@ -177,11 +194,22 @@ class CircularLinkedList {
     return undefined
   }
 
+  removeAfterHead() {
+    let node = this.#head.next
+    this.#head.next = node.next
+    this.#searchByValue[node.element] = null
+    return node
+  }
+
+  getElementByValue(elm) {
+    return !!this.#searchByValue[elm] ? this.#searchByValue[elm] : -1
+  }
+
   indexOf(elm) {
-    let current = this.head
+    let current = this.#head
     let index = -1
 
-    const temp = this.head.element
+    const temp = this.#head.element
 
     while (current) {
       if (elm === current.element) {
@@ -203,20 +231,24 @@ class CircularLinkedList {
     return this.indexOf(elm) !== -1
   }
 
+  isPresentBySearch(elm) {
+    return !!this.#searchByValue[elm]
+  }
+
   getHead() {
-    return this.head
+    return this.#head
   }
 
   setHead(head) {
-    this.head = head
+    this.#head = head
   }
 
   toString() {
-    let current = this.head
+    let current = this.#head
     let out = ''
 
     //Keep track of the head
-    const temp = this.head.element
+    const temp = this.#head.element
 
     while (current) {
       //If head is the next element then break
--- puzzles/day-23/part2.js
+++ puzzles/day-23/part2.js
@@ -3,7 +3,7 @@ const cupStack = require('../../modules/cup-stack.js')
 function solve(input) {
   const cups = input[0].split('').map(x => parseInt(x))
 
-  return cupStack.runExtremeSimulation(cups, 100)
+  return cupStack.runExtremeSimulation(cups, 10000000)
 }
 
 module.exports = { solve }
