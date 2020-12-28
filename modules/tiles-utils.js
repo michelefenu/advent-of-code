24472457342866fa0153f63d3927f7b33af7968a
Michele Fenu <michelefenu@gmail.com>
2020-12-28 18:22:26 +0100

    Solve Day 23 - WORST CODE EVER

--- modules/tiles-utils.js
+++ modules/tiles-utils.js
@@ -33,33 +33,31 @@ const buildPhoto = (tiles) => {
   const cornerTile = tilesMap.get(borderTileId)
   cornerTile.done = true
 
-  const puzzle = [[cornerTile]]
+  let puzzle = [[cornerTile]]
 
   let row = 0;
   let col = 0;
 
-  while(true) {
+  while (true) {
 
     const currentTile = puzzle[row][col]
     let matchingTile
     while (true) {
       const rightBorder = currentTile.borders[1]
-
       matchingTile = findMatchingTile(currentTile.id, rightBorder, tilesMap)
+
       tilesMap.set(matchingTile.id, flipV(tilesMap.get(matchingTile.id)))
+      matchingTile = findMatchingTile(currentTile.id, rightBorder, tilesMap)
       if (rightBorder === matchingTile.borders[3])
         break;
 
       tilesMap.set(matchingTile.id, flipH(tilesMap.get(matchingTile.id)))
       matchingTile = findMatchingTile(currentTile.id, rightBorder, tilesMap)
-
       if (rightBorder === matchingTile.borders[3])
         break;
 
-
       tilesMap.set(matchingTile.id, rotateCC(tilesMap.get(matchingTile.id)))
       matchingTile = findMatchingTile(currentTile.id, rightBorder, tilesMap)
-
       if (rightBorder === matchingTile.borders[3])
         break;
 
@@ -87,7 +85,7 @@ const buildPhoto = (tiles) => {
 
         tilesMap.set(newStartedTile.id, flipH(tilesMap.get(newStartedTile.id)))
         newStartedTile = findMatchingTile(startCurrentTile.id, bottomBorder, tilesMap)
-        
+
         if (bottomBorder === newStartedTile.borders[0])
           break;
 
@@ -112,9 +110,105 @@ const buildPhoto = (tiles) => {
   }
 
   // Here we have the puzzle!!!
+  //console.log(printPuzzle(puzzle).join('\n'))
 
+  const snakePattern = [
+    '..................#.'.split(''),
+    '#....##....##....###'.split(''),
+    '.#..#..#..#..#..#...'.split('')
+  ]
+
+
+  puzzle = printPuzzle(puzzle)
+  let seaMonsters = 0
+
+  while(true) {
+    seaMonsters = findSeaMonsters(snakePattern, puzzle)
+    if(seaMonsters !== 0) {
+      break
+    }
+
+    puzzle = flipVPuzzle(puzzle)
+    seaMonsters = findSeaMonsters(snakePattern, puzzle)
+    if(seaMonsters !== 0) {
+      break
+    }
+
+    puzzle = flipHPuzzle(puzzle)
+    seaMonsters = findSeaMonsters(snakePattern, puzzle)
+    if(seaMonsters !== 0) {
+      break
+    }
+
+    puzzle = rotatePuzzle(puzzle)
+    seaMonsters = findSeaMonsters(snakePattern, puzzle)
+    if(seaMonsters !== 0) {
+      break
+    }
+  }
+  const snakeHashes = snakePattern.join('').split('').filter(x => x === '#').length * seaMonsters
+
+  const totalHashes = puzzle.join('').split('').filter(x => x === '#').length
+console.log(totalHashes - snakeHashes)
 }
 
+const findSeaMonsters = (snakePattern, puzzle) => {
+  let seaMonsters = 0
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[0].length; j++) {
+      
+      let found = true
+      for (let k = 0; k < snakePattern.length; k++) {
+        for (let l = 0; l < snakePattern[0].length; l++) {
+          if(snakePattern[k][l] === '#') {
+            if(puzzle[i+k] && puzzle[i+k][j+l] === '#') {
+              
+            } else {
+              found = false
+            }
+          }
+        }
+      }
+
+      if(found) {
+        seaMonsters++
+      }
+    }
+  }
+
+  return seaMonsters
+}
+
+const printPuzzle = (puzzle) => {
+
+  puzzle = removeBorders(puzzle)
+
+  const output = []
+  const tileHeight = puzzle[0][0].tile.length
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let tileRow = 0; tileRow < tileHeight; tileRow++) {
+      let out = ''
+      for (let j = 0; j < puzzle[0].length; j++) {
+        out += puzzle[i][j].tile[tileRow]
+      }
+      output.push(out)
+
+    }
+  }
+
+  return output
+}
+
+const removeBorders = (puzzle) => {
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[0].length; j++) {
+      puzzle[i][j].tile = puzzle[i][j].tile.slice(1, puzzle[i][j].tile.length - 1)
+      puzzle[i][j].tile = puzzle[i][j].tile.map(x => x.split('').slice(1, x.length - 1).join(''))
+    }
+  }
+
+  return puzzle
+}
 
 const findMatchingTile = (id, border, tiles) => {
   return [...tiles.values()].filter(x => x.id !== id).filter(x => matchesAnyBorder(border, x.borders))[0]
@@ -136,6 +230,21 @@ const rotateCC = (tile) => {
   return rotatedTile
 }
 
+const rotatePuzzle = (puzzle) => {
+  let rotatedPuzzle = []
+
+  for (let i = 0; i < puzzle[0].length; i++) {
+    let row = ''
+    for (let line of puzzle) {
+      row = line[i] + row
+    }
+    rotatedPuzzle.push(row)
+  }
+
+
+  return rotatedPuzzle
+}
+
 const flipV = (tile) => {
   const flippedTile = { id: tile.id, tile: [], borders: [] }
   flippedTile.tile = tile.tile.reverse()
@@ -144,11 +253,19 @@ const flipV = (tile) => {
   return flippedTile
 }
 
+const flipVPuzzle = (puzzle) => {
+  return puzzle.reverse()
+}
+
 const flipH = (tile) => {
   const flippedTile = { id: tile.id, tile: [], borders: [] }
   flippedTile.tile = tile.tile.map(x => x.split('').reverse('').join(''))
   flippedTile.borders = getBorders(flippedTile.tile)
   return flippedTile
+}
+
+const flipHPuzzle = (puzzle) => {
+  return puzzle.map(x => x.split('').reverse('').join(''))
 }
 
 const toScreen = (tile) => {
