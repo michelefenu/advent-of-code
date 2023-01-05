91bc7df91825105dbde95b30c774803ac7d7f47e
Michele Fenu <michelefenu@gmail.com>
2023-01-06 00:43:08 +0100

    Add Day 3

--- Advent of Code 2022/puzzles/day-03/part2.js
+++ Advent of Code 2022/puzzles/day-03/part2.js
@@ -1,6 +1,14 @@
+const day3 = require('../../modules/day3');
+
 function solve(input) {
-   throw 'Not solved, yet'
- }
- 
- module.exports = { solve }
- 
+
+  const groups = [];
+
+  for (let i = 0; i < input.length - 2; i = i + 3) {
+    groups.push([input[i].split(''), input[i + 1].split(''), input[i + 2].split('')])
+  }
+
+  return day3.part2(groups);
+}
+
+module.exports = { solve }
