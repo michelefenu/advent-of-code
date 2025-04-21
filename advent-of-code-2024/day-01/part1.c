75bc1c26a996328249a1cba0f6eb6783d6a99964
michelefenu <michelefenu@gmail.com>
2025-04-22 00:30:53 +0200

    fix tabs

--- advent-of-code-2024/day-01/part1.c
+++ advent-of-code-2024/day-01/part1.c
@@ -1,31 +1,27 @@
-/* 
-Don't forget to redirect input file when executing program
- 
-./part1 < input.txt
- 
-Let's see what I rember of C language. No special tricks here, just a JavaScript dev trying to figuring out ho to read from stdin and cycle 2 arrays
-
-*/
+/*
+ * Redirect input file to stdin ./part1 < input.txt
+ *
+ * Let's see what I rember of C language. No special tricks here, just a JavaScript dev trying to figuring out ho to read from stdin and cycle 2 arrays. 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_LIST_SIZE 2000
 
-
 void sort_array(int *arr, int size) {
   int swapped = 1;
-  while(swapped) {
-		swapped = 0;
-		for(int i=0; i<size - 1; i++) {
-			if(arr[i]>arr[i+1]) {
-				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
-				swapped = 1;
-			}
-		}
-	}
+  while (swapped) {
+    swapped = 0;
+    for (int i = 0; i < size - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+	int temp = arr[i];
+	arr[i] = arr[i + 1];
+	arr[i + 1] = temp;
+	swapped = 1;
+      }
+    }
+  }
 }
 
 int main(void) {
@@ -34,16 +30,16 @@ int main(void) {
   int lp = 0;
   int distance = 0;
 
-  while(scanf("%d %d", &list1[lp], &list2[lp]) != EOF) {
-		lp++;
+  while (scanf("%d %d", &list1[lp], &list2[lp]) != EOF) {
+    lp++;
   }
 
-	sort_array(list1, lp);
-	sort_array(list2, lp);
+  sort_array(list1, lp);
+  sort_array(list2, lp);
 
-  for(int i=0; i<lp; i++) {
-		distance = distance + abs(list1[i] - list2[i]);
-	}
+  for (int i = 0; i < lp; i++) {
+    distance = distance + abs(list1[i] - list2[i]);
+  }
 
-	printf("The distance is %d", distance);
+  printf("The distance is %d", distance);
 }
