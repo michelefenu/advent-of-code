c4c9e3c99831ba4d325b548a14a1241b74298071
michelefenu <michelefenu@gmail.com>
2025-04-23 00:48:33 +0200

    Solve Day 2 part 1

---
+++ advent-of-code-2024/day-02/part1.c
@@ -0,0 +1,65 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(void) {
+  FILE *file; char buffer[84];
+
+  file = fopen("input.txt", "r");
+
+  int safe_rows = 0;
+  
+  while(fgets(buffer, 84, file) != NULL) {
+    char *token;
+    token = strtok(buffer, " ");
+    
+    int is_safe = TRUE;    
+    int direction = 0;
+    int level;
+    
+    sscanf(token, "%d", &level);
+
+    while (token != NULL) {
+	int prev = level;	
+
+        token = strtok(NULL, " ");
+
+	if(token == NULL) {
+	  break;
+	}
+
+	sscanf(token, "%d", &level);
+	
+	if(direction == 0) {
+	  if(prev > level) {
+            direction = -1;
+	  } else if(prev < level) {
+	    direction = 1;
+	  } else {
+	    is_safe = FALSE;
+	    break;
+	  }
+	}
+
+	if(direction == -1 && (prev < level || abs(prev - level) < 1 || abs(prev - level) > 3)) {
+	  is_safe = FALSE;
+	  break;
+        } else if(direction == 1 && (prev > level || abs(prev - level) < 1 || abs(prev - level) > 3)) {
+	  is_safe = FALSE;
+	  break;
+	} 
+	// printf("%d ", level);
+    
+    }
+
+    if(is_safe) {
+      safe_rows++;
+    }
+    // printf("%d\n", is_safe);
+  }
+  
+  printf("Total safe lines: %d", safe_rows);
+}
