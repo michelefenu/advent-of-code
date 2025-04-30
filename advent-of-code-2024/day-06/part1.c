cd1f20382fd6d141b44f6fe30ddf5b963efebb20
michelefenu <michelefenu@gmail.com>
2025-04-30 09:02:46 +0200

    Add day 6

---
+++ advent-of-code-2024/day-06/part1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    int x;
+    int y;
+} Coordinate;
+
+const Coordinate direction_map[] = {
+    [0]   = {-1, 0},
+    [90]  = {0, 1},
+    [180] = {1, 0},
+    [270] = {0, -1}
+};
+
+int main(void) {
+  FILE* fp;
+  char buf[255];
+  int rows = 0;
+  int cols = 0;
+
+  fp = fopen("input.txt", "r");
+
+  while(fgets(buf, sizeof(buf), fp) != NULL) {
+    rows++;
+  }
+
+  while(buf[cols] != '\n') { cols++; }
+  
+  rewind(fp);
+
+  char** board = malloc(rows*sizeof(char*));  
+  for(int i=0; i<rows; i++) {
+    fgets(buf, sizeof(buf), fp);
+    board[i] = malloc(cols*sizeof(char));
+    int k = 0;
+    while(buf[k] != '\n') { 
+      board[i][k] = buf[k];
+      k++; 
+    }
+  }
+
+  int guard_x;
+  int guard_y;
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<cols; j++) {
+      printf("%c", board[i][j]);
+      if(board[i][j] == '^') {
+	guard_x = i;
+	guard_y = j;
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  int angle = 0;
+  Coordinate direction = direction_map[angle]; // North
+  while(guard_x > 0 && guard_x < rows && guard_y > 0 && guard_y < cols) {
+    board[guard_x][guard_y] = 'X';
+    
+    if(guard_x+direction.x < 0 || guard_x+direction.x >= rows || guard_y+direction.y < 0 || guard_y+direction.y >= cols) {
+      break;
+    }
+    if(board[guard_x+direction.x][guard_y+direction.y] != '#') {
+      guard_x = guard_x + direction.x;
+      guard_y = guard_y + direction.y;
+    } else {
+      angle = (angle + 90) % 360;
+      direction = direction_map[angle];
+    }
+  } 
+
+  int visited_cells = 0;
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<cols; j++) {
+      printf("%c", board[i][j]);
+      if(board[i][j] == 'X') {
+	visited_cells++;
+      }
+    }
+    printf("\n");
+  }
+
+  printf("Total visited cells: %d", visited_cells);  
+}
---
+++ advent-of-code-2024/day-06/part2.c
@@ -0,0 +1,1 @@
+// To solve the second part we can count the positions where the guard passes more than N times:w:1
